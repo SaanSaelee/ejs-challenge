a705247ed939b6d912af2219d3440e1dc322a1f3
pennyflipper <saan.sinh.saelee@gmail.com>
2022-06-14 16:34:48 -0700

    updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,18 +19,59 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
+// Sends a reqest to the server and responds beack to you with the home route
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent });
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
+// Sends a request to the server to get the About page
 app.get("/about", function (req, res) {
   res.render("about", { about: aboutContent });
 });
 
+// Sends a request to the server to get the Contact page
 app.get("/contact", function (req, res) {
   res.render("contact", { contact: contactContent });
 });
 
-app.listen(3000, function () {
+// Sends a request to the server to get the Compose page
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+// Sends a post request to the Compose page
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
+});
+
+// Server listening
+app.listen(3000, function (err) {
+  if (err) console.log(err);
   console.log("Server started on port 3000");
 });
